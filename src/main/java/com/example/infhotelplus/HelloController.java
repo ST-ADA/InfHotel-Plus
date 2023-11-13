e5d0f6e612951bf65d9d44bede3f958b638ecd81
Sebastiaan <mab.tromper1@ad-academie.nl>
2023-11-13 14:16:08 +0100

    Initial commit with all start files.

---
+++ src/main/java/com/example/infhotelplus/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.infhotelplus;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("FOR THE EMPEROR!");
+    }
+}
